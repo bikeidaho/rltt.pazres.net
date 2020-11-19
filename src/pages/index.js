c0627869536d461c089c8ac79a3221b7b5ad228f
bikeidaho <43679674+bikeidaho@users.noreply.github.com>
2020-11-19 15:43:29 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const IndexPage = () => (
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>RLTT</h1>
     <p>Welcome to Remote Learning Tech Tips.</p>
-    <p>This is the first pull request for this project.</p>
+    <p>This is the first or many pull request for this project.</p>
     
   </Layout>
 )
