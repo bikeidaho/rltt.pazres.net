12ee3967c96a1ffc325510ba5567efb2cfcafd62
bikeidaho <43679674+bikeidaho@users.noreply.github.com>
2020-11-19 15:16:54 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,9 +11,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>RLTT</h1>
-    <p>Welcome to Remote Learning Tech Tips.</p><br>
-    We must be able to build without issues.</br>
-
+    <p>Welcome to Remote Learning Tech Tips.</p>
     <p>
       Follow{' '}
       <a href="https://www.gatsbyjs.org/tutorial/ecommerce-tutorial/">
