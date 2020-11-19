ef861d8c9c7ee03e0a7cca1f4e9a9d9c03de6b73
bikeidaho <43679674+bikeidaho@users.noreply.github.com>
2020-11-19 16:20:55 -0700

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from 'react'
 const Header = ({ siteTitle }) => (
   <div
     style={{
-      background: `rebeccapurple`,
+      background: `DarkCyan`,
       marginBottom: `1.45rem`,
     }}
   >
